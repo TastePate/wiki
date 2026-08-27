4e9b48667e07962d9179fe944be65b325d61262b
TastePate <pavelarz14@gmail.com>
2026-08-27 19:13:59 +0400

    add: path for article adding

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = "encyclopedia"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:page_title>", views.wiki, name="wiki")
+    path("wiki/", views.index, name="index"),
+    path("wiki/<str:page_title>", views.wiki, name="wiki"),
+    path("add/", views.add, name="add"),
 ]
